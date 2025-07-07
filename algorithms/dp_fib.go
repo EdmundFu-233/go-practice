0e32f3fddf5b872b0db270f43aec01d2a1cd4866
Zhihao Fu <120171892+EdmundFu-233@users.noreply.github.com>
2025-07-07 13:45:00 +0000

    update: dp fib

---
+++ algorithms/dp_fib.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func fib(n int) int {
+	if n <= 1 {
+		return n
+	}
+	dp := make([]int, n+1)
+	dp[0], dp[1] = 0, 1
+	for i := 2; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
+
+func fibOptimized(n int) int {
+	if n <= 1 {
+		return n
+	}
+	a, b := 0, 1
+	for i := 2; i <= n; i++ {
+		a, b = b, a+b
+	}
+	return b
+}
+
+func main() {
+	fmt.Println(fib(10))
+	fmt.Println(fibOptimized(10))
+}
