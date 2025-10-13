ac20ab30bbf50bc01cbc6ed04bacbb250c87642f
Zhihao Fu <120171892+EdmundFu-233@users.noreply.github.com>
2025-10-13 13:42:00 +0000

    add: switch

---
+++ basics/switch.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("Weekend!")
+	default:
+		fmt.Println("Weekday")
+	}
+}
