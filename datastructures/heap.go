f12d4b3f2eadd3019da264a601d9692b0b8836b8
Zhihao Fu <120171892+EdmundFu-233@users.noreply.github.com>
2026-01-20 22:43:00 +0000

    add: heap

---
+++ datastructures/heap.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type MinHeap []int
+
+func (h MinHeap) Len() int           { return len(h) }
+func (h MinHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *MinHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MinHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
+}
+
+func main() {
+	h := &MinHeap{3, 1, 4, 1, 5}
+	heap.Init(h)
+	heap.Push(h, 2)
+	for h.Len() > 0 {
+		fmt.Printf("%d ", heap.Pop(h))
+	}
+	fmt.Println()
+}
