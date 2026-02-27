0f5a6bd2a8965fa1773e92ef970376d276b66543
Zhihao Fu <120171892+EdmundFu-233@users.noreply.github.com>
2026-02-27 17:00:00 +0000

    update: routing

---
+++ http/routing.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Home Page")
+	})
+	mux.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, `{"status":"ok"}`)
+	})
+	fmt.Println("Server on :8080")
+	http.ListenAndServe(":8080", mux)
+}
